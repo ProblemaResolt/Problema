0d3729351ccbb117f4c46839362a7122c071520d
ProblemaResolt <boner.engineer@gmail.com>
2020-06-11 15:48:56 +0900

    6modified

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,6 +1,15 @@
-import { Elm } from './Main.elm'
-import './sass/main.scss'
+'use strict';
 
-Elm.Main.init({
-    node: document.getElementById('elm')
+require("./styles.scss");
+
+const {Elm} = require('./Main');
+var app = Elm.Main.init({flags: 6});
+
+app.ports.toJs.subscribe(data => {
+    console.log(data);
 })
+// Use ES2015 syntax and let Babel compile it for you
+var testFn = (inp) => {
+    let a = inp + 1;
+    return a;
+}
